d3780c450105cb15d8501f1df5d84475d0a61407
Sudarshan <105160284+Sudarshan50@users.noreply.github.com>
2025-03-10 11:51:55 +0530

    Add CORS support and improve server startup logging in server.js...

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ import dotenv from 'dotenv';
 import { dbConnect } from './lib/db.js';
 import AdminRouter from './routes/adminRoutes.js';
 import masterRouter from './routes/masterRoute.js';
+import cors from 'cors';
 
 
 const app = e();
 dotenv.config();
 const port = process.env.PORT || 3000;
 app.use(e.json());
+app.use(cors());
+app.use(e.urlencoded({ extended: true }));
+
 
 
 
@@ -17,8 +21,8 @@ app.use('/api/master',masterRouter);
 
 
 
-dbConnect().then(()=>{
-    app.listen(port, ()=>{
-        console.log(`Server is running on port ${port}`);
-    })
-})
+dbConnect().then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  });
